4df6d7d18e0f6e7c75b731f93abc6f50020c5919
Matt White <mawhi@microsoft.com>
2020-06-05 00:05:36 +0100

    Add remediation task and identity assignment

--- managementgroup-childmg-policyassignments.tf
+++ managementgroup-childmg-policyassignments.tf
@@ -30,3 +30,15 @@ resource "azurerm_policy_assignment" "deploy-log-analytics-childmg" {
 }
 PARAMETERS
 }
+
+resource "azurerm_role_assignment" "deploy-log-analytics-childmg" {
+  scope                = azurerm_policy_assignment.deploy-log-analytics-childmg.scope
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_policy_assignment.deploy-log-analytics-childmg.identity[0].principal_id
+}
+
+resource "azurerm_policy_remediation" "deploy-log-analytics-childmg" {
+  name                 = "remediatedeploydiagloganalytics"
+  scope                = azurerm_policy_assignment.deploy-log-analytics-childmg.scope
+  policy_assignment_id = azurerm_policy_assignment.deploy-log-analytics-childmg.id
+}
--- managementgroups.tf
+++ managementgroups.tf
@@ -1,12 +1,12 @@
 resource "azurerm_management_group" "root" {
   display_name = "Root"
-  subscription_ids = [
-    "814a2d9d-752d-4ecb-bb2a-fc0321152d4b"
-  ]
 }
 
 resource "azurerm_management_group" "childmg" {
   display_name = "Child"
-  name = "0ed2a5ff-bbc9-4d67-bd7e-cf311da6742a"
+  name         = "0ed2a5ff-bbc9-4d67-bd7e-cf311da6742a"
+  subscription_ids = [
+    "814a2d9d-752d-4ecb-bb2a-fc0321152d4b"
+  ]
   parent_management_group_id = azurerm_management_group.root.id
 }
