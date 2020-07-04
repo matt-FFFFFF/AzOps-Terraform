0448c45e469514b7125c621e64e35715cd5e4b28
Matt White <matt.white@microsoft.com>
2020-07-04 22:54:15 +0100

    add la and aa

--- tf-0000-platform/variables.tf
+++ tf-0000-platform/variables.tf
@@ -1,7 +1,22 @@
 variable "my_object_id" {
-  type=string
+  type = string
 }
 
-variable "my_subscription_id" {
-  type=string
+variable "mgmt_subscription_id" {
+  type = string
+}
+
+variable "defaultlocation" {
+  type    = string
+  default = "westeurope"
+}
+
+variable "log_analytics_rg_name" {
+  type = string
+  default = "ES-mgmt"
+}
+
+variable "log_analytics_name_suffix" {
+  type = string
+  default = "-"
 }
