d071d82d48ea09e0475a00ce29e363e62886de2a
Matt White <matt.white@microsoft.com>
2020-07-07 23:31:20 +0000

    bootstrap script ready for action

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,10 +5,11 @@ set -e
 # Defaults
 LOCATION='westeurope'
 SP_NAME='AzOpsTF'
+SP_ACTION_NAME='AzOpsAction'
 SP_ROOT_ROLE_ASSIGNMENT='Management Group Contributor'
 TF_RG_NAME='ES-tf'
 TF_STATE_FILE_NAME='es.tfstate'
-TF_STORAGE_ACCT_NAME="estf$RANDOM$RANDOM"
+TF_RANDOM_NAME="estf$RANDOM$RANDOM"
 TF_STORAGE_ACCT_SKU='Standard_GZRS'
 TF_STORAGE_CONTAINER_NAME="tfbackend"
 
@@ -22,11 +23,12 @@ exit_abnormal() {
   usage
 }
 
-SUBSCRIPTION_ID=$(az account list --query '[?isDefault].id' --output tsv)
-echo "Using subscription id: $SUBSCRIPTION_ID"
+SUBSCRIPTION=$(az account list --query '[?isDefault]' --output json)
+SUBSCRIPTION_ID=$(echo $SUBSCRIPTION | jq -r .[].id)
+echo "Using subscription id: $(echo $SUBSCRIPTION | jq -r '.[].id')"
 echo "Using location: $LOCATION"
 
-if [ ! $SUBSCRIPTION_ID ]; then
+if [ ! "$SUBSCRIPTION_ID" ]; then
   exit_abnormal 'Not logged in with az cli or no subscriptions'
   usage
   exit 1
@@ -39,25 +41,33 @@ export AZURE_CORE_OUTPUT=none
 az configure --defaults group=$TF_RG_NAME location=$LOCATION \
              --scope local
 
+echo "Creating resource group $TF_RG_NAME"
+az group create --name $TF_RG_NAME 
+
 echo "Creating service principal and assigning $SP_ROOT_ROLE_ASSIGNMENT role at tenant root scope '/'"
 SP=$(az ad sp create-for-rbac -n $SP_NAME \
                                --role "$SP_ROOT_ROLE_ASSIGNMENT" \
                                --scopes '/' \
                                --output json)
 
-echo "Waiting for the new SPN to appear in the Azure AD Graph"
-while [ ! "$SP_OBJECT" ]; do
-  echo '.'
+echo "Creating service principal and assigning Reader role at resource group: $TF_RG_NAME"
+SP_ACTION=$(az ad sp create-for-rbac -n $SP_ACTION_NAME \
+                               --role "Reader" \
+                               --scopes "/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$TF_RG_NAME" \
+                               --sdk-auth \
+                               --output json)
+
+echo "Waiting for the new service principals to appear in the Azure AD Graph"
+while [ ! "$SP_OBJECT" ] || [ ! "$SP_ACTION_OBJECT" ]; do
+  printf '.'
   SP_OBJECT=$(az ad sp show --id "http://$SP_NAME" --output json)
+  SP_ACTION_OBJECT=$(az ad sp show --id "http://$SP_ACTION_NAME" --output json)
   sleep 5
 done
+printf '\n'
 
-
-echo "Creating resource group $TF_RG_NAME"
-az group create --name $TF_RG_NAME 
-
-echo "Creating storage account $TF_STORAGE_ACCT_NAME"
-az storage account create --name $TF_STORAGE_ACCT_NAME \
+echo "Creating storage account $TF_RANDOM_NAME"
+az storage account create --name $TF_RANDOM_NAME \
                           --https-only \
                           --kind StorageV2 \
                           --sku $TF_STORAGE_ACCT_SKU
@@ -66,29 +76,66 @@ echo "Adding 'Storage Blob Data Contributor' role assignment for SPN"
 az role assignment create --role 'Storage Blob Data Contributor' \
                           --assignee-object-id $(echo $SP_OBJECT | jq -r .objectId) \
                           --assignee-principal-type ServicePrincipal \
-                          --scope "/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$TF_RG_NAME/providers/Microsoft.Storage/storageAccounts/$TF_STORAGE_ACCT_NAME"
+                          --scope "/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$TF_RG_NAME/providers/Microsoft.Storage/storageAccounts/$TF_RANDOM_NAME"
 
 echo "Adding 'Storage Blob Data Contributor' role assignment for $(echo $ADMIN_USER | jq -r .userPrincipalName)"
 TOBEREMOVED=$(az role assignment create --role 'Storage Blob Data Contributor' \
                           --assignee-object-id $(echo $ADMIN_USER | jq -r .objectId) \
                           --assignee-principal-type User \
-                          --scope "/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$TF_RG_NAME/providers/Microsoft.Storage/storageAccounts/$TF_STORAGE_ACCT_NAME" \
+                          --scope "/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$TF_RG_NAME/providers/Microsoft.Storage/storageAccounts/$TF_RANDOM_NAME" \
                           --output json)
 
 echo "Creating blob container for Terraform backend"
-az storage container create --account-name $TF_STORAGE_ACCT_NAME \
+az storage container create --account-name $TF_RANDOM_NAME \
                             --auth-mode login \
                             --name $TF_STORAGE_CONTAINER_NAME
+
+echo "Creating key vault $TF_RANDOM_NAME"
+az keyvault create --name $TF_RANDOM_NAME
+
+echo "Adding key vault access policy for $(echo $ADMIN_USER | jq -r .userPrincipalName)"
+az keyvault set-policy --name $TF_RANDOM_NAME \
+                       --resource-group $TF_RG_NAME \
+                       --object-id $(echo $ADMIN_USER | jq -r .objectId) \
+                       --secret-permissions get list set
+
+echo "Adding kay vault access policy for action/pipeline service principal"
+az keyvault set-policy --name $TF_RANDOM_NAME \
+                       --resource-group $TF_RG_NAME \
+                       --object-id $(echo $SP_ACTION_OBJECT | jq -r .objectId) \
+                       --secret-permissions get list
+
+echo "Creating secrets: ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_TENANT_ID & ARM_SUBSCRIPTION_ID"
+az keyvault secret set --vault-name $TF_RANDOM_NAME \
+                --name arm-client-id \
+                --value $(echo $SP | jq -r .appId)
+az keyvault secret set --vault-name $TF_RANDOM_NAME \
+                --name arm-client-secret \
+                --value $(echo $SP | jq -r .password)
+az keyvault secret set --vault-name $TF_RANDOM_NAME \
+                --name arm-subscription-id \
+                --value $SUBSCRIPTION_ID
+az keyvault secret set --vault-name $TF_RANDOM_NAME \
+                --name arm-tenant-id \
+                --value $(echo $SUBSCRIPTION | jq -r '.[].tenantId')
+
 
 az configure --defaults group='' location='' \
              --scope local
 
+echo "Removing 'Storage Blob Data Contributor' role assignment for $(echo $ADMIN_USER | jq -r .userPrincipalName)"
 az role assignment delete --ids $(echo $TOBEREMOVED | jq -r .id )
 
 echo "Creating Terraform backend file"
 cp backend.hcl.example backend.hcl
 sed -i "s/myrg/$TF_RG_NAME/" backend.hcl
-sed -i "s/mystorageaccount/$TF_STORAGE_ACCT_NAME/" backend.hcl
+sed -i "s/mystorageaccount/$TF_RANDOM_NAME/" backend.hcl
 sed -i "s/mystatecontainer/$TF_STORAGE_CONTAINER_NAME/" backend.hcl
 sed -i "s/mybackendkey.tfstate/$TF_STATE_FILE_NAME/" backend.hcl
 
+echo "Creating service principal for the GitHub Action/Azure DevOps pipeline (copy into your action/pipeline)"
+echo "-------------------------------"
+echo $SP_ACTION
+echo "-------------------------------"
+
+unset AZURE_CORE_OUTPUT
