b55f810b7c77eb8299ddfbdd26f0ba5fa2b87bd1
Matt White <matt.white@microsoft.com>
2020-07-14 20:46:25 +0000

    include vhub example

--- tf-001-connectivity/virtualwan.tf
+++ tf-001-connectivity/virtualwan.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "vwan" {
+/* resource "azurerm_resource_group" "vwan" {
   name     = var.vwan_rg_name
   location = var.default_location
 }
@@ -7,4 +7,4 @@ resource "azurerm_virtual_wan" "vwan" {
   name                = var.vwan_name
   resource_group_name = azurerm_resource_group.vwan.name
   location            = azurerm_resource_group.vwan.location
-}
+} */
